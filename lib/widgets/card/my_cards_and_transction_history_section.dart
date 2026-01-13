143e01cb4140209989cd0abde77b28b729a7b2dd
Youssef Mansour <yousaf83000@gmail.com>
2026-01-13 21:36:07 +0200

    feat: add custom card components and integrate expandable page view

---
+++ lib/widgets/card/my_cards_and_transction_history_section.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/card/my_cards_section.dart';
+import 'package:responsive_dashboard/widgets/general/custom_background_container.dart';
+
+class MyCardsAndTransctionHistorySection extends StatelessWidget {
+  const MyCardsAndTransctionHistorySection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackgroundContainer(
+        child: Column(
+      children: [
+        MyCardsSection(),
+        Divider(
+          height: 40,
+          color: Color(0xffF1F1F1),
+        ),
+        // TrasnctionHistory(),
+      ],
+    ));
+  }
+}
